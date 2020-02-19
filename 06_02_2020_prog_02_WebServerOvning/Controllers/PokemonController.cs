9cf8a31a6f41bac028b87fdc3f396624a8d9dd3b
BeepBoopError <emil0131@gmail.com>
2020-02-19 14:53:34 +0100

    Initial commit

---
+++ 06_02_2020_prog_02_WebServerOvning/Controllers/PokemonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06_02_2020_prog_02_WebServerOvning.Controllers
+{
+    [Route("api/pokemon")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        public static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "Spicelord", strength = 100},
+            new Pokemon() {Name = "Ricelord", strength = 69},
+            new Pokemon() {Name = "Icelord", strength = 120}
+        };
+
+
+
+
+        [HttpGet]
+        public ActionResult MyPokemonGet()
+        {
+            /*Pokemon p = new Pokemon();
+            p.Name = "Ekans";*/
+
+            return Ok(pokemons);
+        }
+
+
+    }
+}
---
+++ 06_02_2020_prog_02_WebServerOvning/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class Pokemon
+{
+    public int strength;
+
+    private string name;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value.ToUpper();
+        }
+    }
+}
+
